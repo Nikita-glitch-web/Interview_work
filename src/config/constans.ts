d0d6f5b6088003627f24439850f1ce95c41e7d40
Nick <nikitalytvynov0506@gmail.com>
2025-03-10 20:30:01 +0200

    Deploy!

--- src/config/constans.ts
+++ src/config/constans.ts
@@ -1,3 +1,4 @@
 export const API_BASE_URL =
   "https://frontend-test-assignment-api.abz.agency/api/v1";
-export const IMG_URL = process.env.NODE_ENV === "production" ? "/test-app/" : ""
+export const IMG_URL =
+  process.env.NODE_ENV === "production" ? "/Interview_work/" : "";
